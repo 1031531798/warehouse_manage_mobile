ce0d8eb4eb71f2a9add9fd1e89deadf3029fe6d3
1031531798 <1031531798@qq.com>
2023-02-19 23:45:46 +0800

    style(商品页面): 优化布局 添加code clip 显示

--- src/pages/storage/goods/list.tsx
+++ src/pages/storage/goods/list.tsx
@@ -7,6 +7,8 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import {Fragment, useMemo, useState} from "react";
 import {GoodsProps} from "@/pages/storage/types";
+import {Chip} from "@mui/material";
+import SubtitlesIcon from '@mui/icons-material/Subtitles';
 const GoodsList = () => {
     const [goodsListData, setGoodsListData] = useState<GoodsProps[]>([
         {id: "1111", goodsImage:"", goodsName: "商品名称", goodsNum: 12,goodsCode: "122222", price: 22 },
@@ -17,6 +19,16 @@ const GoodsList = () => {
     function renderSearch () {
         return <input />
     }
+    function renderGoodsTitle (goods: GoodsProps) {
+       return (
+           <div className={'flex flex-row justify-between'}>
+               <span>
+                   {goods.goodsName}
+               </span>
+               <Chip icon={<SubtitlesIcon />} color={'primary'} variant={'outlined'} label={"No." + goods.goodsCode} />
+           </div>
+       )
+    }
     const renderList = useMemo(() => {
         return goodsListData.map(item => {
             return (<ListItem alignItems="flex-start" key={item.id}>
@@ -24,7 +36,7 @@ const GoodsList = () => {
                     <Avatar alt={item.goodsName} src={item.goodsImage}/>
                 </ListItemAvatar>
                 <ListItemText
-                    primary={item.goodsName}
+                    primary={renderGoodsTitle(item)}
                     secondary={
                         <Fragment>
                             <Typography
@@ -34,6 +46,9 @@ const GoodsList = () => {
                                 color="text.primary"
                             >
                                 编号：{item.goodsCode}
+                            </Typography>
+                            <Typography>
+                                价格：{item.price}
                             </Typography>
                         </Fragment>
                     }
